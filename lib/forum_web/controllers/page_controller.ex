ae1cd939ee95973432a374d7eabe714db8a3c141
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-03-28 14:10:51 +0300

    created user migration

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,13 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Alice", age: 30, email: "alice@email.com"},
+      %{id: 2, name: "Bob", age: 31, email: "bob@email.com"},
+      %{id: 3, name: "Charlie", age: 32, email: "charlie@email.com"}
+    ]
+    json(conn, %{users: users})
+  end
 end
